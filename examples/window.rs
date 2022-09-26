14ee4dbaefcefcab2d519ca842bd99c323ab4565
RustingRobot <rusting.robot.scratch@gmail.com>
2022-09-26 21:09:36 +0200

    simple loop

--- examples/window.rs
+++ examples/window.rs
@@ -1,40 +1,10 @@
-#![windows_subsystem = "windows"]
-
-extern crate sdl2;
-
-use std::{thread, time::Duration};
-use sdl2::pixels::Color;
-use sdl2::event::Event;
+use lilac;
 
 fn  main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem.window("window", 400, 400)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
-
-    //window.set_bordered(false);
-    let mut canvas = window.into_canvas().build().unwrap();
-    canvas.set_draw_color(Color::RGB(100, 149, 237));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    ..
-                } => break 'running,
-                _ => {}
-            }
-        }
-        canvas.set_draw_color(Color::RGB(100, 149, 237));
-        canvas.clear();
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    let mut window = lilac::init();
+    println!("this is a test");
+    while !window.exit() {
+        window.update();
+        window.render();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,57 @@
-pub fn test(){
-    println!("using da library!");
+#![windows_subsystem = "windows"]
+
+extern crate sdl2;
+
+use std::time::Duration;
+use sdl2::EventPump;
+use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::render::WindowCanvas;
+
+pub struct Window{
+    canvas: WindowCanvas,
+    //video: VideoSubsystem,
+    events: EventPump
+}
+
+impl Window {
+    pub fn update(&self){
+
+    }
+
+    pub fn render(&mut self){
+        self.canvas.set_draw_color(Color::RGB(100, 149, 237));
+        self.canvas.clear();
+        self.canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+
+    pub fn exit(&mut self) -> bool{
+        for event in self.events.poll_iter() {
+            match event {
+                Event::Quit { .. } => return true,
+                _ => {}
+            }
+        }
+        false
+    }
+}
+
+pub fn init() -> Window{
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let win = video_subsystem.window("window", 400, 400)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    //window.set_bordered(false);
+    let temp_canvas = win.into_canvas().build().unwrap();
+    let event_pump = sdl_context.event_pump().unwrap();
+    Window{
+        canvas: temp_canvas,
+        //video: video_subsystem,
+        events: event_pump
+    }
 }
